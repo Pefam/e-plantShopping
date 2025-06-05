aa3c8f395469352260a3dcdff1af78ffee4c70c7
Michael <michael@pefam.com>
2025-06-05 18:58:43 -0400

    CartItems component

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -8,28 +8,35 @@ const CartItem = ({ onContinueShopping }) => {
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
-  const calculateTotalAmount = () => {
- 
+    const calculateTotalAmount = () => {
+    let total = 0;
+    cart.forEach(item => total += item.quantity * parseFloat(item.cost.substring(1)));
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
-
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+   return item.quantity * parseFloat(item.cost.substring(1));
+  };
+
+    const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
   return (
